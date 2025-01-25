54ab455996b1d2a6e1cd9538d927f2c69b260001
joudi-saeidan <156447467+joudi-saeidan-work@users.noreply.github.com>
2025-01-25 09:21:28 +0000

    feat(expand-card): only render card model when user clicks on expand

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,8 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { Button } from "@/components/ui/button";
+import { Maximize2 } from "lucide-react";
 
 interface CardItemProps {
   data: Card;
@@ -10,8 +12,9 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
-  // use card modal
+  // we need the card model to track the state of a card
   const cardModal = useCardModal();
+  const { onOpen } = cardModal;
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -19,11 +22,22 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
-          role="button"
-          className="truncate border-2 border-transparent hover:border-black/30 py-2 px-3 text-sm bg-white rounded-md  shadow-sm "
-          onClick={() => cardModal.onOpen(data.id)}
+          role="content"
+          // TODO: condiser updating the card items
+          className="relative flex flex-col justify-between border-2 border-transparent hover:border-black/30 py-2 px-3 text-sm bg-white rounded-md  shadow-sm "
         >
-          {data.title}
+          <span className="whitespace-pre-wrap break-words overflow-hidden text-ellipsis">
+            {data.title}
+          </span>
+          <Button
+            size="sm"
+            variant="ghost"
+            className="absolute bottom-2 right-2 h-4 w-4 text-neutral-600"
+            onClick={() => onOpen(data.id)}
+            title="Expand"
+          >
+            <Maximize2></Maximize2>
+          </Button>
         </div>
       )}
     </Draggable>
