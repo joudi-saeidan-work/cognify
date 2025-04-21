c7e17ec792a0cdfd3340fa9d19fa814198e40261
joudi-saeidan <156447467+joudi-saeidan-work@users.noreply.github.com>
2025-04-21 05:50:33 +0100

    fix(bug): fix dnd bug

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -23,7 +23,7 @@ const BoardIdPage = async ({ params }: BoardIdPageProps) => {
   });
 
   return (
-    <div className="p-4 h-full overflow-x-auto">
+    <div className="p-4 h-full">
       <ListContainer boardId={params.boardId} data={lists} />
     </div>
   );
